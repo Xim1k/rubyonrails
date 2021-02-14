76af308d29c59f163de17b22e0cbcb33532f881e
nvmikhailov <Nikita.Mikhailov@noveogroup.com>
2021-02-14 18:31:07 +0700

    Allow to choose quantity for request

--- app/controllers/request_controller.rb
+++ app/controllers/request_controller.rb
@@ -1,9 +1,9 @@
 class RequestController < ApplicationController
     def create
         @user = current_user
-        @product = Product.find(params[:format])
+        @product = Product.find(params[:product_id])
         @freeOperator = helpers.findFreeOperator
-        request=Request.new(status: Request::IN_PROGRESS, user:current_user, operator:@freeOperator, product:@product, quantity: 1)
+        request=Request.new(status: Request::IN_PROGRESS, user:current_user, operator:@freeOperator, product:@product, quantity: params[:quantity])
         request.save!
 
         redirect_to product_index_path
